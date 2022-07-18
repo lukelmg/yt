fa0f925de90ddb3a42b09c56fabe3aa93fd41ca5
lukelmg <lukegutman14@gmail.com>
2022-07-18 13:28:07 -0400

    fixed subchannel

--- subschannel.js
+++ subschannel.js
@@ -1,21 +1,19 @@
 let delay = 1; // the delay inbetween adds
-let multiplier = 1; // increase to make faster
+let multiplier = 5; // increase to make faster
 let startCount = 2000; // starting sub count
 let endCount = 10000; // end sub count
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 async function load() {
-    var i;
-    var output;
-    for (i = 0; i < (endCount - startCount + 1); i = i + (123 * multiplier)) {
-        output = (i + startCount);
-        document.getElementById("subscriber-count").innerHTML = numberWithCommas(output) + ' subscribers';
-        await timer(delay); // then the created Promise can be awaited
-    }
+    var output = startCount;
     while (output != endCount) {
-        output++;
-        document.getElementById("subscriber-count").innerHTML = numberWithCommas(output) + ' subscribers';
+        output = output + multiplier;
+        document.getElementById("subscriber-count").innerHTML = output + ' subscribers';
+        await timer(delay)
+    }
+    if (output > endCount) {
+        output = endCount;
     }
 }
 
