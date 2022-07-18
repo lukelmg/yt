350cbb8412e955963134d1beb5103d0458e3d81f
lukelmg <lukegutman14@gmail.com>
2022-07-18 13:37:12 -0400

    try now

--- views.js
+++ views.js
@@ -1,4 +1,4 @@
-let delay = 1; // the delay inbetween adds
+let delay = 5; // the delay inbetween adds
 let multiplier = 5; // increase to make faster
 let startCount = 2000; // starting sub count
 let endCount = 10000; // end sub count
@@ -9,7 +9,7 @@ async function load() {
     var output = startCount;
     while (output != endCount) {
         output = output + multiplier;
-        document.getElementsByClassName("style-scope yt-formatted-string bold")[0].innerHTML = numberWithCommas(output) + ' views';
+        document.getElementById("style-scope yt-formatted-string bold")[0].innerHTML = numberWithCommas(output) + ' views';
         await timer(delay)
     }
         output = endCount;
